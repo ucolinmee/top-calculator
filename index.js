0c898d27f4fa18871d6cd6035ef198b12b3b44ce
ucolinmee <colinjmeehan23@gmail.com>
2024-02-10 19:12:44 +0800

    Create operate function to check which operator was used and call the matching function

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+    if (operator === "+")
+        return add(num1, num2);
+    if (operator === "-")
+        return subtract(num1, num2);
+    if (operator === "*")
+        return multiply(num1, num2);
+    if (operator === "/")
+        return divide(num1, num2);
+}
+
+var num1;
+var operator;
+var num2;
