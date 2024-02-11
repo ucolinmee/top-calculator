03a032dbdf9e6420b983365022ca5309936f02a5
ucolinmee <colinjmeehan23@gmail.com>
2024-02-11 21:17:29 +0800

    Add event listeners so the buttons are displayed on the screen

--- index.js
+++ index.js
@@ -28,3 +28,29 @@ function operate(num1, operator, num2) {
 var num1;
 var operator;
 var num2;
+
+function populateDisplay(value) {
+    var display = document.querySelector(".display-text");
+    var currentDisplay = display.textContent;
+    display.textContent = currentDisplay + value;
+}
+
+function clearDisplay() {
+    var display = document.querySelector(".display-text");
+    display.textContent = "";
+}
+
+var buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        populateDisplay(e.target.innerHTML);
+    })
+})
+
+var clear = document.querySelector(".clear-button");
+clear.addEventListener("click", () => {
+    clearDisplay();
+})
+
+// TODO: Add calculator functionality when "=" is pressed
+// TODO: function validateOperation() : checks validity of operation
